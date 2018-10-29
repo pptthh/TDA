e634bccb09a6b8c43609139e43d2b91f008f07df
Péter Tóth <peter_toth1@epam.com>
2018-10-29 11:07:42 +0100

    change TS lint rule "->'

--- fe/src/App.tsx
+++ fe/src/App.tsx
@@ -5,15 +5,15 @@ import logo from './logo.svg';
 import Button from './component/Button';
 
 const App = () =>
-<div className="App">
-  <header className="App-header">
-    <h1 className="App-title"><img src={logo} className="App-logo" alt="logo"/>
+<div className='App'>
+  <header className='App-header'>
+    <h1 className='App-title'><img src={logo} className='App-logo' alt='logo'/>
       Welcome your to ToDo app React</h1>
   </header>
-  <p className="App-intro">
+  <p className='App-intro'>
     To get started, edit <code>src/App.tsx</code> and save to reload.
-    If you havent don it yet.
-    <Button/>
+    If you havent don it yet. <br />
+    <Button name='button' label='button'/>
   </p>
 </div>;
 
