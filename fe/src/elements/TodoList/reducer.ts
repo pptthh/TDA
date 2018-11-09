8a4734860906adb5b93af9738a4d08c0f008335c
Péter Tóth <peter_toth1@epam.com>
2018-11-09 18:41:25 +0100

    prepare for todo list

--- fe/src/elements/TodoList/reducer.ts
+++ fe/src/elements/TodoList/reducer.ts
@@ -1,3 +1,29 @@
-const TodoListReducer = ;
+import { IActions } from '../../actions';
+import { LOG } from '../../utils';
+import ITodoListState from './state';
+
+const init: ITodoListState = {
+    list: [],
+};
+
+interface IAddNoteSwtch {
+    state: ITodoListState;
+    payload: unknown;
+}
+
+const swtch = {};
+
+const TodoListReducer = (state: ITodoListState = init, {type, payload}: IActions<unknown>): ITodoListState => {
+    LOG('', payload, '\t', type, 'TestCounterswtch');
+    try {
+        return swtch[type]({state, payload} as IAddNoteSwtch);
+    } catch (e) {
+        if (e instanceof TypeError) {
+            return state;
+        } else {
+            throw(e);
+        }
+    }
+};
 
 export default TodoListReducer;
--- fe/src/state/index.ts
+++ fe/src/state/index.ts
@@ -1,9 +1,11 @@
 import IAddNoteState from '../elements/AddNote/state';
 import ICounterState from '../elements/TestCounter/state';
+import ITodoListState from '../elements/TodoList/state';
 
 interface IState {
     addNoteState: IAddNoteState;
     counterState: ICounterState;
+    todoList: ITodoListState;
 }
 
 export default IState;
