ce73a4b179cacf67b8ebbad380e2360513fac590
Péter Tóth <peter_toth1@epam.com>
2018-11-13 09:40:44 +0100

    optimyze switch

--- fe/src/reducers/createReducer.ts
+++ fe/src/reducers/createReducer.ts
@@ -9,23 +9,9 @@ export interface ISwitch<T> {
     [s: string]: ({state, payload}: ICase<T>) => T;
 }
 
-export const createReducer = <T>(
-    Switch: ISwitch<T>,
-    stateInit: T,
-) => (
-    (
-        state: T = stateInit,
-        {type, payload}: IActions<unknown>,
-    ): T => {
-        try {
-            return Switch[type]({state, payload}) as T;
-        } catch (e) {
-            if (e instanceof TypeError) {
-                return state;
-            }
-            throw(e);
-        }
-    }
-);
+export const createReducer =
+    <T>(Switch: ISwitch<T>, stateInit: T) =>
+        (state: T = stateInit, {type, payload}: IActions<unknown>): T =>
+            Switch.hasOwnProperty(type) ? Switch[type]({state, payload}) : state;
 
 export default createReducer;
